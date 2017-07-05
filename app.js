0e4a996136ca529fcd05d54525af2cc7e16186cf
bwielk <bwielk@gmail.com>
2017-07-05 17:33:43 +0200

    Toggle works

--- app.js
+++ app.js
@@ -11,25 +11,42 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-
-
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+activePlayer = 0;
 
 var x = document.querySelector('#score-1');
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.dice').style.display = 'none';
+
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	var dice = Math.floor(Math.random()*6) + 1;
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-'+ dice + ".png";
-
-
-
+	if(dice !== 1){
+		roundScore +=dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	}else{
+		activePlayer  === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+		document.getElementById('current-0').textContent = "0";
+		document.getElementById('current-1').textContent = "0";
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		document.querySelector('.dice').style.display = "none";
+	}
 });
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
